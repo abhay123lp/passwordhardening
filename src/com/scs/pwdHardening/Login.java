4117a1054324dc3f52e0d133986838616f3d4975
the77thsecret <the77thsecret@gmail.com>
2011-02-22 16:32:22 +0000

    Description: Mandatory Questions

--- src/com/scs/pwdHardening/Login.java
+++ src/com/scs/pwdHardening/Login.java
@@ -27,32 +27,34 @@ public class Login {
 		System.out.print("Enter password : ");
 		String password = scanner.nextLine();
 		
-		int answeredQuestionsCount = 0;
+		int answeredQuestionsCount = 0, curIndex = 1;
 		
 		for(Category currCategory : Category.values()){
 			if(answeredQuestionsCount <= 3){
-				boolean skipped = askQuestionFromList(Questionnaire.ALL_QUESTIONS.get(currCategory));
-				answeredQuestionsCount += skipped ? answeredQuestionsCount : ++answeredQuestionsCount;
+				boolean canSkipQuestion = Category.values().length - curIndex > 3 - answeredQuestionsCount;
+				boolean skipped = askQuestionFromList(Questionnaire.ALL_QUESTIONS.get(currCategory), canSkipQuestion);
+				answeredQuestionsCount = skipped ? answeredQuestionsCount : ++answeredQuestionsCount;
 			}
 			else{
 				break;			
 			}
+			++curIndex;
 		}
 	}
 	
-	private static boolean askQuestionFromList(ArrayList<Question> categoryQuestions){
+	private static boolean askQuestionFromList(ArrayList<Question> categoryQuestions, boolean canSkipQuestion){
 		int randIndex = Utility.getRandomInteger();
 		Question q = categoryQuestions.get(randIndex);
 		System.out.println(q.getQuestion());
 		System.out.println("Your choices are : ");
 		categoryQuestions.remove(randIndex);	// We have asked this question. Remove it so that we don't repeat it.
 		
-		int userResponseIndex = getResponse(q.getChoices()) - 1;
+		int userResponseIndex = getResponse(q.getChoices(), canSkipQuestion) - 1;
 		
 		return loginService.evaluateResponse(userResponse, q, userResponseIndex);
 	}
 	
-	private static int getResponse(String [] choices){
+	private static int getResponse(String [] choices, boolean canSkipQuestion){
 		int chosenResponse, ch = 1;
 		for(String choice : choices){
 			System.out.println(ch++ + ") " + choice);
@@ -60,14 +62,22 @@ public class Login {
 		System.out.print("Response : ");
 		try{
 			chosenResponse = Integer.parseInt(scanner.nextLine());
+			if(chosenResponse < 1 || chosenResponse > choices.length){
+				System.out.println("Invalid choice. Please choose from one of the below responses");
+				chosenResponse = getResponse(choices, canSkipQuestion);
+			}
+			if(!canSkipQuestion && choices[chosenResponse - 1].equalsIgnoreCase("SKIP_QUESTION")){
+				System.out.println("Cannot skip any more questions. Please choose one of the below responses");
+				return getResponse(choices, canSkipQuestion);
+			}
 		}
 		catch(NumberFormatException nfe){
 			System.out.println("Invalid choice. Please choose from one of the below responses");
-			return getResponse(choices);
+			return getResponse(choices, canSkipQuestion);
 		}
 		if(chosenResponse < 1 || chosenResponse > choices.length){
 			System.out.println("Invalid choice. Please choose from one of the below responses");
-			return getResponse(choices);
+			return getResponse(choices, canSkipQuestion);
 		}
 		return chosenResponse;
 	}
